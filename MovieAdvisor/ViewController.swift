9ebc6bf649c7bf229fab41245dbfb052c541593a
Anastasia Shepureva <anastasya.shepureva@gmail.com>
2022-02-16 12:06:00 +0300

    Initial Commit

---
+++ MovieAdvisor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAdvisor
+//
+//  Created by Анастасия Шепурева on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
